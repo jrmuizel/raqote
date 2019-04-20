ec1a0101ec9cfe9bc6c5a051cc34a9f1eca4d85d
Jeff Muizelaar <jrmuizel@gmail.com>
2019-04-20 15:28:28 -0400

    Add a simple test that at least runs through the code

--- src/rasterizer.rs
+++ src/rasterizer.rs
@@ -600,3 +600,14 @@ impl<'a> Rasterizer<'a> {
 			// printf("comparisons: %d\n", comparisons);
 		}
 	}
+
+#[test]
+fn simple_test() {
+	let mut arena = Arena::new();
+	let mut r = Rasterizer::new(&mut arena, 200, 200);
+	r.add_edge(Point { x: 50., y: 50. }, Point { x: 100., y: 70. }, false, Point { x: 0., y: 0. });
+	r.add_edge(Point { x: 100., y: 70. }, Point { x: 110., y: 150. }, false, Point { x: 0., y: 0. });
+	r.add_edge(Point { x: 110., y: 150. }, Point { x: 40., y: 180. }, false, Point { x: 0., y: 0. });
+	r.add_edge(Point { x: 40., y: 180. }, Point { x: 50., y: 50. }, false, Point { x: 0., y: 0. });
+	r.rasterize();
+}
